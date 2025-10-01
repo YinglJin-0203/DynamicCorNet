2c8b74adb94dd733e1dd3be0515f1a2fb2b20da0
YinglJin-0203 <jiny8@nih.gov>
2025-10-01 10:36:26 -0400

    check before furlough

--- helpers/DeleteLater.R
+++ helpers/DeleteLater.R
@@ -9,3 +9,12 @@ expand_grid(time = 1:10, id = 1:10) %>%
   pivot_longer(3:4) %>%
   ggplot(aes(x=time, y=value, fill=name, colour = name, group = interaction(time, name)))+
   geom_boxplot(position = "dodge2")
+
+df <- read.csv("data/AppData.csv")
+max(df$Testosterone_N, na.rm = T)
+df[which.max(df$Testosterone_N), ] # participant 520102 at week 36
+
+df %>%
+  ggplot(aes(x=Week, y=Testosterone_N, group = Week))+
+  geom_boxplot(outlier.size = 0.5)
+ggsave("images/testosterone.jpeg")
